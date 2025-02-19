4df36c10ec7f64dbb35bc5b0ccff02552863fc6a
RahulSarkar <bluephoenix00995@gmail.com>
2025-02-19 13:29:54 +0530

    feat: tree collaps

--- realty_reflex/hooks.py
+++ realty_reflex/hooks.py
@@ -57,7 +57,7 @@ doctype_list_js = {
 }
 
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-doctype_tree_js = {"Task" : "public/js/wbsss.js"}
+doctype_tree_js = {"Task" : "public/js/wbs_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
 # Svg Icons
